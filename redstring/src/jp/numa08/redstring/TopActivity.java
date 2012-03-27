c2e64f9a16276e7df1ba98fbe6143aa0b8bf03c0
numa08 <n511287@gmail.com>
2012-03-28 00:43:37 +0900

    first commit

---
+++ redstring/src/jp/numa08/redstring/TopActivity.java
@@ -0,0 +1,13 @@
+package jp.numa08.redstring;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TopActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
